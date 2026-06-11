9b3e97f51819b79c96f6a75bb9ddefbace5b7fbe
shri-honeytha <shrihoneythareddy2006@gmail.com>
2026-06-11 16:01:51 +0530

    initila commit

---
+++ server.js
@@ -0,0 +1,92 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken";
+
+const SECRET="mysecret123"
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.listen(8081, () => {
+  console.log("Server started");
+});
+
+mongoose.connect("mongodb://localhost:27017/mytestdb1")
+
+const userSchema = new mongoose.Schema({
+  name:String,
+  email: {
+    type: String,
+    unique: true,
+  },
+  password: String,
+  role: {
+    type: String,
+    default: "user",
+  },
+});
+
+const userModel = mongoose.model("User", userSchema);
+
+app.post("/users/register", async (req, res) => {
+ 
+
+    const {name,email,password,role} = req.body;
+    const hashedPassword=await bcrypt.hash(password,10)
+    const user=await userModel.create({
+        name, email, password:hashedPassword,role
+    })
+    res.json(user)
+   
+} )
+
+app.post("/users/login", async (req, res) => {
+  
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({ email });
+    if(user){
+        const chkPassword=await bcrypt.compare(password,user.password)
+        if(chkPassword){
+            const obj={
+id: user._id,
+name:user.name,
+    email:user.email,role:user.role
+            }
+            const token=await jwt.sign(obj,SECRET,{expiresIn:"1h"})
+            res.json({...obj,token})
+        }
+        else{
+            res.json({message:"Invalid Password"})
+        }
+    
+    }
+    else{
+        res.json({message:"user not found"})
+    }
+});
+
+const authenticate=async (req,res,next)=>{
+
+    try{
+    const authHeader=req.headers.authorization
+    const token=authHeader.split(" ")[1] //[0] is Bearer and [1] is abc from authHeader, it will extract the token abc
+   const user=await jwt.verify(token,SECRET)
+   req.user=user
+   next()
+    }
+    catch(error){
+res.json({message:"Unauthorized"})
+    }
+  
+
+}
+
+app.get("/users", authenticate,async (req, res) => {
+    const users = await userModel.find();
+    res.json(users);
+});
